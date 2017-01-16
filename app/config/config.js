d523a6bef8436851ddf6835f31ea195f6870cabe
Luay Suarna <muhamad.luay@gmail.com>
2017-01-16 10:05:00 +0700

    Tidy up structure app

--- app/config/config.js
+++
@@ -1,9 +0,0 @@
-var Todo = require('Todo');
-
-Todo.config([
-  'RestangularProvider',
-  function(RestangularProvider){
-    RestangularProvider.setBaseUrl('http://localhost:3000/api/v1');
-  }
-]);
-
